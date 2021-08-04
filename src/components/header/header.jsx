624dabd2cd8500e0f8aa6efd61be13c9f3c5f9af
Your Name <you@example.com>
2021-08-04 18:45:43 +0300

    Version 0.2.3

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -48,7 +48,7 @@ const Header = ({logo}) => {
                         <ul className="header__nav-items">
                             <li className="header__nav-item"><a href="services.html" className="header__nav-link">Услуги</a></li>
                             <li className="header__nav-item"><a href="loan.html" className="header__nav-link">Рассчитать кредит</a></li>
-                            <li className="header__nav-item header__nav-item--active"><a href="#" className="header__nav-link">Конвертер валют</a></li>
+                            <li className="header__nav-item header__nav-item--active"><a href="converter.html" className="header__nav-link">Конвертер валют</a></li>
                             <li className="header__nav-item"><a href="contacts.html" className="header__nav-link">Контакты</a></li>
                         </ul>
                     </nav>
